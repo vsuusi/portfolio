8770eda1035a88d5df9607a62269f11131d9b78b
Santeri Uusitalo <vs.uusitalo@gmail.com>
2024-02-08 21:21:46 +0200

    remade animations with js cuz browsers didnt supper full css

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,15 +1,20 @@
+import { useInView } from "react-intersection-observer";
 import "./Bio.css";
 
 const Bio = () => {
+
+    const { ref: bio1, inView: bio1IsVisible} = useInView();
+    const { ref: bio2, inView: bio2IsVisible} = useInView();
+
     return(
         <div className="bio-container">
-            <div className="bio-1">
-                <p>My name is <strong>Santeri Uusitalo</strong>, a third year software engineering student and I am curiously seeking 
-                    a trainee position to apply and expand my programming skills. 
-                <br/>Eager to learn through hands-on experiences,
-                <br/>I am ambitious about developing my expertise in cutting-edge technologies.</p>
-            </div>
-            <div className="bio-2">
+                <div ref={bio1} className={`bio-1 ${bio1IsVisible ? "animate-bio" : ""}`}>
+                    <p>My name is <strong>Santeri Uusitalo</strong>, a third year software engineering student and I am curiously seeking 
+                        a trainee position to apply and expand my programming skills. 
+                    <br/>Eager to learn through hands-on experiences,
+                    <br/>I am ambitious about developing my expertise in cutting-edge technologies.</p>
+                </div>
+                <div ref={bio2} className={`bio-2 ${bio2IsVisible ? "animate-bio" : ""}`}>
                 <p>Currently most interesting to me are front end developing, UI's, UX and AI, but I am open to other career paths too! 
                 <br/>You can find my resume below next to LinkedIn and GitHub.</p>
             </div>
