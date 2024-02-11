9f404aa932d677fb43f1146274b9d3c58ee50bce
Santeri Uusitalo <vs.uusitalo@gmail.com>
2024-02-11 12:44:42 +0200

    adjusted bio and color improvement to scrollbar

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -11,11 +11,10 @@ const Bio = () => {
                 <div ref={bio1} className={`bio-1 ${bio1IsVisible ? "animate-bio" : ""}`}>
                     <p>My name is <strong>Santeri Uusitalo</strong>, a third year software engineering student and I am curiously seeking 
                         a trainee position to apply and expand my programming skills. 
-                    <br/>Eager to learn through hands-on experiences,
-                    <br/>I am ambitious about developing my expertise in cutting-edge technologies.</p>
+                    <br/>Eager to learn through hands-on experiences, I am ambitious about developing my expertise in cutting-edge technologies.</p>
                 </div>
                 <div ref={bio2} className={`bio-2 ${bio2IsVisible ? "animate-bio" : ""}`}>
-                <p>Currently most interesting to me are front end developing, UI's, UX and AI, but I am open to other career paths too! 
+                <p>Currently most interesting to me are front end developing, UI's, UX and AI, but almost anything in this field gets me excited to learn more! 
                 <br/>You can find my resume below next to LinkedIn and GitHub.</p>
             </div>
         </div>
